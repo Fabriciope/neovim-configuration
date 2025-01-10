0b9191f50805e41c789ae30c26492d9e4893ce69
Fabrício Pereira Alves <fabricioalvespa@gmail.com>
2025-01-10 00:02:51 -0300

    feat: update to my own configs

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,13 +13,28 @@ return {
     end,
   },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+        "intelephense"
+      }
+    }
+  },
+
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+        "html", "css", "javascript",
+        "php", "golang"
+  		},
+  	},
+  },
 }
